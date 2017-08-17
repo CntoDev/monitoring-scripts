10c545c77712e5f3042bb4c8de3c77e543223b90
Enrico Ghidoni <enricoghdn@gmail.com>
2017-08-17 11:15:31 +0200

    Invalid URL tests

--- tests/test_http_monitor.py
+++ tests/test_http_monitor.py
@@ -3,15 +3,32 @@ import pytest
 from cnto_incident_detection import http_monitor
 from cnto_incident_detection import nagios_common
 
+
+def assert_nagios_exit(expected_code, exit_info, stdout):
+    """Common assertions to test Nagios compatibility"""
+    if not isinstance(expected_code, nagios_common.Codes):
+        raise ValueError('expected_code must be an instance of cnto_incident_detection.nagios_common.Codes')
+
+    assert exit_info == expected_code.value
+    assert stdout.startswith(expected_code.name + ':')
+
+
+def invalid_url(url, capfd):
+    """Assert that status is UNKNOWN with wrong URL"""
+    with pytest.raises(SystemExit) as exit_info:
+        http_monitor.main(url)
+
+    out, err = capfd.readouterr()
+    assert_nagios_exit(nagios_common.Codes.UNKNOWN, exit_info.value.code, out)
+
 def test_invalid_url_invalid_protocol(capfd):
     """Assert that status is UNKNOWN with wrong URL protocol"""
     url = 'htt://foo.bar'
 
-    with pytest.raises(SystemExit) as exit_info:
-        http_monitor.main(url)
+    invalid_url(url, capfd)
 
-    assert exit_info.value.code == nagios_common.Codes.UNKNOWN.value
+def test_invalid_url_invalid_domain(capfd):
+    """Assert that status is UNKNOWN with wrong URL domain"""
+    url = 'http://foo'
 
-    out, err = capfd.readouterr()
-
-    assert out.startswith('UNKNOWN:')
+    invalid_url(url, capfd)
